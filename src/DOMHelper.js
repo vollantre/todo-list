387c2cb2d7b0214ee6602d20de08ef24b6bf2154
Diego Brito <britodiego.tech@gmail.com>
2020-10-23 11:14:32 -0400

    create addProjects function in the DOMHelper

--- src/DOMHelper.js
+++ src/DOMHelper.js
@@ -2,20 +2,47 @@ export default (() => {
   const closeModal = modalId => document.getElementById(modalId).classList.remove("is-active");
   const openModal = modalId => document.getElementById(modalId).classList.add("is-active");
 
-  const newTaskBtn = document.getElementById('new-task-btn');
-  const newProjectBtn = document.getElementById('new-project-btn');
-
   return {
     //add click events listeners
     addEvents: () => {
+      const newProjectBtn = document.getElementById('new-project-btn');
+      const newTaskBtn = document.getElementById('new-task-btn');
+
       newProjectBtn.addEventListener("click", () => openModal("project-modal"));
       newTaskBtn.addEventListener("click", () => openModal("task-modal"));
-      
-      //add listeners to cancel button of each modal
-      Array.from(document.querySelectorAll("a#modal-close")).forEach(node => {
+
+      const modals = document.querySelectorAll("a#modal-close");
+
+      modals.forEach(node => {
         const modal = node.parentNode.parentNode.parentNode.parentNode.parentNode;
         node.addEventListener("click", () => closeModal(modal.id));
-      })
+      });
+
+
+      //const projectList = document.querySelectorAll("#project-list>li");
+//
+      //projectList.forEach(node => {
+      //  node.firstChild(a => a.addEventListener)
+      //});
+    },
+
+    renderProject: project => {
+
+    },
+
+    addProjects: projects => {
+      const projectList = document.getElementById("project-list");
+
+      projects.forEach(project => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        a.innerText = project.name;
+
+        li.id = `project-${project.id}`;
+        li.appendChild(a);
+        projectList.appendChild(li);
+      });
     }
   };
 })();
--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ const projects = [{
 }];
 
 DOMHelper.addEvents();
+DOMHelper.addProjects(projects);
